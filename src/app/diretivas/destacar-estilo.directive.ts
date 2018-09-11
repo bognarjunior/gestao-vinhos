d657f2e5cdd59d8b6b3d02c7227b08ee15a830dd
Bognar Junior <bognar_junior@yahoo.com.br>
2018-09-10 21:09:40 -0300

    Implementando diretiva
    Impelementando diretiva

--- src/app/diretivas/destacar-estilo.directive.ts
+++ src/app/diretivas/destacar-estilo.directive.ts
@@ -1,10 +1,27 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
-  selector: '[appDestacarEstilo]'
+  selector: '[destacarEstilo]'
 })
 export class DestacarEstiloDirective {
 
-  constructor() { }
+  constructor(
+    private elementRef: ElementRef
+  ) { }
 
+  @HostListener('mouseenter') onMouseEnter() {
+    this.transformar('bold', 'uppercase');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.transformar();
+  }
+
+  private transformar(
+    fontWeight: string = null,
+    textTransform: string = null
+  ) {
+    this.elementRef.nativeElement.style.fontWeight = fontWeight;
+    this.elementRef.nativeElement.style.textTransform = textTransform;
+  }
 }
